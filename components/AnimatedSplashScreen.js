15ce171f6cbd5523625ad7002a4770f1959105a9
Oguzhan Albayrak <oguzhanalbayrak@Mac.fritz.box>
2025-03-06 23:49:27 +0100

    first commit

--- components/AnimatedSplashScreen.js
+++ components/AnimatedSplashScreen.js
@@ -5,7 +5,7 @@ import LottieView from 'lottie-react-native'
 const AnimatedSplashScreen = () => {
   return (
     <View style={styles.container}>
-        <LottieView style={{height:150,width:150}} autoPlay loop source={require("../assets/lottie/pomCoffeeAnim.json")} />
+        <LottieView style={{height:120,width:120}} autoPlay loop source={require("../assets/lottie/pomCoffeeAnim.json")} />
     </View>
   )
 }
